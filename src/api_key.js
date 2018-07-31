70a610c812febdf059a090b2df24ecbe71536d5c
Michael Salvatore <msalvatore22@gmail.com>
2018-07-31 13:44:17 -0400

    first commit

---
+++ src/api_key.js
@@ -0,0 +1,2 @@
+var api_key = "1c5cc83cd286417"
+var client_secret = "7ca6e4100b4e006eb8d8876eb85ad51505300296"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,65 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/searchbar';
+import Fetch from 'whatwg-fetch'
+const API_KEY = '1c5cc83cd286417';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce some HTML.
+// Downwards Data Flow - the most parent component should be responsible for fetching any data.
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = { 
+      images: [],
+      selectedImg: null
+    };
+
+    this.picSearch('star wars')
+    
+  } 
+
+    picSearch(term){
+      var imgurURL = 'https://api.imgur.com/3/gallery/t/';
+
+      return fetch(imgurURL + term, {
+        headers: {
+          'Authorization': 'Client-ID ' + API_KEY
+        }
+      }).then((response) => {
+         return response.json();
+      }).then((data) => {
+        let imgData = []
+
+        for(let img in data.data.items){
+          if(data.data.items[img].images){
+            imgData.push(data.data.items[img])
+          }
+        }
+          console.log(imgData)
+        this.setState({
+          images: imgData,
+          selectedImg: imgData[0]
+        })
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+
+  render () {
+    const picSearch = _.debounce((term) => {this.picSearch(term)}, 600);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={picSearch}/>
+      </div>
+    );
+  }
+}
+
+
+// Take this component's HTML and put it in the DOM
+
+ReactDOM.render(<App />, document.querySelector('.container'));
